81c8ec6765288c796ec8cac899c45028dcb5e2b4
sample2025nit <sample2025nit@gmail.com>
2021-09-14 21:14:37 +0530

    delete operation

--- src/main/java/in/nareshit/raghu/controller/SpecializationController.java
+++ src/main/java/in/nareshit/raghu/controller/SpecializationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import in.nareshit.raghu.entity.Specialization;
 import in.nareshit.raghu.service.ISpecializationService;
@@ -53,5 +54,16 @@ public class SpecializationController {
 		return "SpecializationData";
 	}
 	
+	/**
+	 * 4. Delete by id
+	 */
+	@GetMapping("/delete")
+	public String deleteData(
+			@RequestParam Long id
+			) 
+	{
+		service.removeSpecialization(id);
+		return "redirect:all";
+	}
 	
 }
