453e52a2e76dee12162eadd0581298c3ea0b2fb5
sample2025nit <sample2025nit@gmail.com>
2021-09-14 21:07:10 +0530

    Register Form, validation at entity

--- src/main/java/in/nareshit/raghu/entity/Specialization.java
+++ src/main/java/in/nareshit/raghu/entity/Specialization.java
@@ -22,14 +22,25 @@ public class Specialization {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="spec_id_col")
 	private Long id;
-	
-	@Column(name="spec_code_col")
+
+	@Column(
+			name="spec_code_col",
+			length = 10,
+			nullable = false,
+			unique = true)
 	private String specCode;
 
-	@Column(name="spec_name_col")
+	@Column(
+			name="spec_name_col",
+			length = 60,
+			nullable = false,
+			unique = true)
 	private String specName;
-	
-	@Column(name="spec_note_col")
+
+	@Column(
+			name="spec_note_col",
+			length = 250,
+			nullable = false)
 	private String specNote;
-	
+
 }
