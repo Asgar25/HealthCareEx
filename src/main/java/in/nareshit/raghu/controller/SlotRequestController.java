c961e6f0373e3bbbddba4466d2681969e8b22268
javabyraghu <javabyraghu@gmail.com>
2021-11-08 21:43:36 +0530

    Revoke PDF Setup

--- src/main/java/in/nareshit/raghu/controller/SlotRequestController.java
+++ src/main/java/in/nareshit/raghu/controller/SlotRequestController.java
@@ -13,7 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import in.nareshit.raghu.constants.SlotStatus;
 import in.nareshit.raghu.entity.Appointment;
@@ -26,7 +25,6 @@ import in.nareshit.raghu.service.IPatientService;
 import in.nareshit.raghu.service.ISlotRequestService;
 import in.nareshit.raghu.service.ISpecializationService;
 import in.nareshit.raghu.util.AdminDashboardUtil;
-import in.nareshit.raghu.view.InvoiceSlipPdfView;
 
 @Controller
 @RequestMapping("/slots")
@@ -175,16 +173,6 @@ public class SlotRequestController {
 		util.generatePie(path, list);
 		return "AdminDashboard";
 	}
-	@GetMapping("/invoice")
-	public ModelAndView generateInvoice(
-			@RequestParam Long id
-			) 
-	{
-		ModelAndView m = new ModelAndView();
-		m.setView(new InvoiceSlipPdfView());
-		SlotRequest slotRequest=service.getOneSlotRequest(id);
-		m.addObject("slotRequest", slotRequest);
-		return m;
-	}
+	
 
 }
